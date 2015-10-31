bb000bf22a0cfb3a9010634506d54fcf1e670e14
Robin Nabel <robin.nabel@gmail.com>
2015-10-31 15:13:43 +0000

    Both side panes and content resizing working.

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,39 +1,59 @@
 /**
- * Created by rn30 on 30/10/2015.
+ * Created by Robin Nabel on 30/10/2015.
  */
+
+console.log("Starting to run content script.");
 
 // TODO add left and right side panes
 // Adapted from: http://stackoverflow.com/questions/14290428/how-can-a-chrome-extension-add-a-floating-bar-at-the-bottom-of-pages
 function addSidePanes() {
-    var right = $("<div></div>");
-    var left = $("<div></div>");
+    var right = $('<div id="leftPane"></div>');
+    var left = $('<div id="rightPane"></div>');
 
     function addStyle(el, isLeft) {
-        // TODO detect isLeft flag.
-        el.attr('style', 'position:fixed;\
-                        top:0px;\
-                        left:0px;\
-                        width:30%;\
-                        height:100%;\
-                        background:white;\
-                        box-shadow:inset 0 0 1em black;\
-                        z-index:999999;');
+        el.css({
+            'position': 'fixed',
+            'top': '0px',
+            'width': '20%',
+            'height': '100%',
+            'background': 'white',
+            'z-index': '999999'
+        });
+        if (isLeft) {
+            el.css({
+                'left': '0px',
+                'box-shadow' : 'inset 0 0 1em black'
+            });
+        } else {
+            el.css({
+                'right': '0px',
+                'box-shadow' : 'inset 0 0 1em black'
+            });
+        }
     }
 
     addStyle(left, true);
     addStyle(right, false);
 
     $(document.body).append(left);
+    $(document.body).append(right);
 
     // Wrap existing content in div element.
     //var iframeWrapper = $("<iframe></iframe>");
     //iframeWrapper.attr('src', window.location.href);
     //iframeWrapper.attr('style', 'height: 100%; width: 100%');
     //document.body.appendChild(iframeWrapper);
+
+    $(document.body).width('60%');
+    $(document.body).css('margin-left', '20%');
+    $(document.body).css('position', 'absolute  ');
 }
 
 function entryPoint() {
+    console.log("Entered entry point.");
     addSidePanes();
 }
 
 entryPoint();
+
+console.log("Finished running the file.");
