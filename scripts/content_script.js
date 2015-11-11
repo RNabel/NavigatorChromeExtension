1c87d016d3e4b6935e946a83c1f5faf04a8f39e7
Robin Nabel <rnabel@ucdavis.edu>
2015-11-11 15:02:55 +0000

    Drag and drop implemented, visualization libs added.

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,14 +1,19 @@
 /**
  * Created by Robin Nabel on 30/10/2015.
  */
+var leftPaneIdentifier = "leftPane";
+var leftPaneSelector = "#" + leftPaneIdentifier;
+var rightPaneIdentifier = "rightPane";
+var rightPaneSelector = "#" + rightPaneIdentifier;
+
 
 console.log("Starting to run content script.");
 
-// TODO add left and right side panes
+// --- General functionality. ---
 // Adapted from: http://stackoverflow.com/questions/14290428/how-can-a-chrome-extension-add-a-floating-bar-at-the-bottom-of-pages
 function addSidePanes(leftPaneSize, rightPaneSize) {
-    var right = $('<div id="rightPane"></div>');
-    var left = $('<div id="leftPane"></div>');
+    var right = $('<div id="' + rightPaneIdentifier + '"></div>');
+    var left = $('<div id="' + leftPaneIdentifier + '"></div>');
 
     function addStyle(el, isLeft) {
         el.css({
@@ -16,7 +21,8 @@ function addSidePanes(leftPaneSize, rightPaneSize) {
             'top': '0px',
             'height': '100%',
             'background': 'white',
-            'z-index': '999999'
+            'z-index': '999999',
+            'background-color': '#FAFAFA'
         });
         if (isLeft) {
             el.css({
@@ -36,6 +42,8 @@ function addSidePanes(leftPaneSize, rightPaneSize) {
     // Add placeholder to right sidepane.
     var picUrl = chrome.extension.getURL('assets/tree.png');
     right.append($('<img>').attr('src', picUrl).attr('width', '90%%').css('margin', '5% 5%'));
+
+    left.append($quoteBox);
 
     addStyle(left, true);
     addStyle(right, false);
@@ -78,6 +86,7 @@ function entryPoint() {
     resizeContent(contentSize, $contentDiv);
 
     addSidePanes(leftContentPaneSize, rightContentPaneSize); // TODO add to func signature.
+    setUpLeftPanel();
 }
 
 function getSelectionText() {
@@ -89,6 +98,109 @@ function getSelectionText() {
     }
     return text;
 }
+
+function slideInSidePanes() {
+
+}
+
+
+// --- Functionality of left pane. ---
+function setUpLeftPanel() {
+    // Add the drop listeners.
+    $('#main').on('drop', drop);
+    $('#main').on('dragover', allowDrop);
+    $('body').on('enddrag', startDrag);
+
+    // Event listener for drag; required to find the origin of the drag.
+    function startDrag(ev) {
+        var path = $(ev.originalEvent.path[1]).getPath(); // TODO update code to be resilient.
+        ev.originalEvent.dataTransfer.setData('src', path);
+    }
+
+    // Event listeners for drop methods.
+    function allowDrop(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+
+        var url = window.location.href;
+        var text = ev.originalEvent.dataTransfer.getData("text/plain");
+        var html_data = ev.originalEvent.dataTransfer.getData("text/html");
+        var source_selector = ev.originalEvent.dataTransfer.getData("src");
+
+        setUpInfoBubble(text, html_data, url, source_selector);
+    }
+}
+
+// Function which adds a text bubble to the left pane.
+// dataTransfer - the object passed by the drop event.
+// originUrl - the origin url of the dropped element.
+function setUpInfoBubble(text, html_text, origin_url, source_selector) {
+    console.log("Create new data bubble called with text:\n" + text);
+
+    // Send data to the backend to store.
+    var obj_to_send = {
+        type: "new_snippet",
+        text: text,
+        html: html_text,
+        url: origin_url,
+        selector: source_selector
+    };
+
+    // Send data to backing store.
+    sendMessage(obj_to_send);
+
+    var $box = createQuoteBox();
+    $box.text(text);
+
+    // TODO scroll to element. http://stackoverflow.com/a/9272017/3918512
+}
+
+function createQuoteBox() {
+    // Add text box to left pane.
+    var $quoteBox = $('<div>').css({
+        'background-color': 'white',
+        width: 100,
+        height: 60,
+        border: '1px solid #eee',
+        'margin-left': '5%', 'margin-top': '30%'
+    });
+
+    var quoteUrl = chrome.extension.getURL('assets/quotes.svg');
+    $quoteBox.append($('<img>').attr('src', quoteUrl).attr('width', '7%').css('margin', '1px 1px'));
+    $quoteBox.append($('<p>').attr('id', 'quoteContent'));
+
+    return $quoteBox;
+}
+
+// ---- Miscellaneous functionality. ----
+// Extend jQuery to be able to generate XPaths from JQuery object.
+jQuery.fn.getPath = function () {
+    if (this.length != 1) throw 'Requires one element.';
+
+    var path, node = this;
+    while (node.length) {
+        var realNode = node[0], name = realNode.localName;
+        if (!name) break;
+        name = name.toLowerCase();
+
+        var parent = node.parent();
+
+        var siblings = parent.children(name);
+        if (siblings.length > 1) {
+            name += ':eq(' + siblings.index(realNode) + ')';
+        }
+
+        path = name + (path ? '>' + path : '');
+        node = parent;
+    }
+
+    return path;
+};
+
 
 // Message end-point.
 chrome.runtime.onMessage.addListener(
@@ -105,6 +217,14 @@ chrome.runtime.onMessage.addListener(
 
         }
     });
+function sendMessage(msgObj, responseHandler) {
+    if (responseHandler === undefined) {
+        responseHandler = function () {};
+    }
+    chrome.runtime.sendMessage(msgObj, responseHandler);
+}
+// Message sender.
+
 
 $(document).ready(entryPoint);
 
